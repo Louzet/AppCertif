26ccf3295360427578f253593b5f64da4fba3a25
Louzet <micklouzet@hotmail.fr>
2018-08-03 21:19:24 +0200

    commit n°2
    Beaucoup de modifications sur les routes, et le controller. Une methode view spéciale qui se chargera d'afficher chaque page principales du site.
    Ajout des templates(header, footer) dans le controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$route['default_controller'] = 'network/index';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
@@ -10,4 +10,13 @@ $route['translate_uri_dashes'] = FALSE;
 	My routes
 */
 
-// $route['network'] = 'network/index';
+
+$route['(:any)'] = 'network/view/$1'; 
+/* 	
+	cette méthode me permettra de raccourcir mes URL'S
+	blog représente mon controller
+	view représente ma méthode qui se chargera de généré toutes les pages
+	$1 est la variable qui renvoie toutes les vues vers 'index.php' grace au fichier '.htaccess
+*/
+
+$route['default_controller'] = 'network/view';
--- application/controllers/Network.php
+++ application/controllers/Network.php
@@ -3,8 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Network extends CI_Controller {
 
-	public function index()
+	public function view($page = 'home') // home remplacera la page d'accueil du frameworks
 	{
-		$this->load->view('network/home');
+		$data['title'] = ucfirst($page);
+		$title = $data['title'].'- App Code Igniter';
+
+
+		$this->load->view('templates/_header');	
+		$this->load->view('network/'.$page, $data, $title);
+		$this->load->view('templates/_footer');
+		
 	}
 }
